b6f7d2a656304ee21974d35c6f5b2a8fda468e5e
Dany <dany@Danys-MacBook-Pro.local>
2020-11-10 14:48:35 -0600

    render movies

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: '30f74663',
+      apikey: '',
       s: searchTerm
     }
   });
+
+  if (response.data.Error) {
+    return [];
+  }
 
   return response.data.Search;
 };
@@ -13,7 +17,17 @@ const input = document.querySelector('input');
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
-  console.log(movies);
+
+  for (let movie of movies) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+    <img src="${movie.Poster}" />
+    <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector('#target').appendChild(div);
+  }
 }; 
 input.addEventListener('input', debounce(onInput, 500));
 
