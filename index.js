b4d49c7bdc0db7c3c1dda5a0d3deba43c9252a7c
Dany <dany@Danys-MacBook-Pro.local>
2020-11-10 11:56:32 -0600

    fetch movie data & link apikey

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '',
+      i: 'tt0848228'
+    }
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
